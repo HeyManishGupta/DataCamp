c498b0b039fd187b0a0d0cfe869c2c48c2337930
Manish Kumar Gupta <44649787+YourManishGupta@users.noreply.github.com>
2021-01-17 23:50:10 +0530

    Update customizing data appearence.py

--- 05 - Intro to Data Viz using Matplotlib/1 - Intro to Matplotlib/customizing data appearence.py
+++ 05 - Intro to Data Viz using Matplotlib/1 - Intro to Matplotlib/customizing data appearence.py
@@ -16,10 +16,10 @@ Pass the linestyle key-word argument to use dashed lines for the data from both 
 """
 
 # Plot Seattle data, setting data appearance
-ax.plot(seattle_weather["MONTH"], seattle_weather["MLY-PRCP-NORMAL"], color="b")
+ax.plot(seattle_weather["MONTH"], seattle_weather["MLY-PRCP-NORMAL"], color="b", marker = "o", linestyle = "--")
 
 # Plot Austin data, setting data appearance
-ax.plot(austin_weather["MONTH"], austin_weather["MLY-PRCP-NORMAL"], color="r")
+ax.plot(austin_weather["MONTH"], austin_weather["MLY-PRCP-NORMAL"], color="r", marker="v", linestyle = "--")
 
 # Call show to display the resulting plot
 plt.show()
