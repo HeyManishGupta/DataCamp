83ff17198ff77c8fea64a4e169b38dea02cfd83a
Manish Kumar Gupta <44649787+YourManishGupta@users.noreply.github.com>
2021-01-21 19:44:37 +0530

    Update exploring classic rock songs.sql

--- Data Analyst with SQL/1 - Intro to SQL Server/1 - Selection/exploring classic rock songs.sql
+++ Data Analyst with SQL/1 - Intro to SQL Server/1 - Selection/exploring classic rock songs.sql
@@ -6,7 +6,32 @@ First, let's get familiar with the data.
 Insstruction
 ------------
 Retrieve the song, artist, and release_year columns from the songlist table.
+Make sure there are no NULL values in the release_year column.
+Order the results by artist and release_year.
 */
 
 -- select top (5) * from songlist;
 select song, artist, release_year from songlist;
+-------------
+-- Retrieve the song, artist and release_year columns
+SELECT 
+  song, 
+  artist, 
+  release_year 
+FROM 
+  songlist 
+  -- Ensure there are no missing or unknown values in the release_year column
+where release_year IS NOT NULL;
+--------------
+-- Retrieve the song,artist and release_year columns
+SELECT 
+  song, 
+  artist, 
+  release_year 
+FROM 
+  songlist 
+  -- Ensure there are no missing or unknown values in the release_year column
+WHERE 
+  release_year IS NOT NULL 
+  -- Arrange the results by the artist and release_year columns
+order by artist, release_year;
