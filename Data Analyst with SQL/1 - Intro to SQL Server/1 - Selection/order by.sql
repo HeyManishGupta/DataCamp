01f9b02cc5f8b37eea6cfadae919c821f68f905d
Manish Kumar Gupta <44649787+YourManishGupta@users.noreply.github.com>
2021-01-21 19:20:43 +0530

    Update order by.sql

--- Data Analyst with SQL/1 - Intro to SQL Server/1 - Selection/order by.sql
+++ Data Analyst with SQL/1 - Intro to SQL Server/1 - Selection/order by.sql
@@ -24,14 +24,14 @@ order by event_date;
 --
 -- Select the top 20 rows from description, nerc_region and event_date
 SELECT 
-  TOP (5) description,
-  description, nerc_region, 
+  TOP (20) description,
+  nerc_region, 
   event_date
 FROM 
   grid 
   -- Order by nerc_region, affected_customers & event_date
   -- Event_date should be in descending order
 ORDER BY
-  nerc_region,
+  nerc_region, 
   affected_customers,
   event_date desc;
