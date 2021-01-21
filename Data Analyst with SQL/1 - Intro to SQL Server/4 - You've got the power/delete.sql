23657fe508c463688a923376d58aac0728834deb
Manish Kumar Gupta <44649787+YourManishGupta@users.noreply.github.com>
2021-01-21 23:13:07 +0530

    Create delete.sql

---
+++ Data Analyst with SQL/1 - Intro to SQL Server/4 - You've got the power/delete.sql
@@ -0,0 +1,12 @@
+/*
+You may not have permissions to delete from your database, but it is safe to practice it here in this course!
+
+Remember - there is no confirmation before deleting. When you execute the statement, the record(s) are deleted immediately. Always ensure you test with a SELECT and WHERE in a separate query to ensure you are selecting and deleting the correct records. If you forget so specify a WHERE condition, you will delete ALL rows from the table.
+
+Instructions
+------------
+Hit 'Submit Answer' to run the query and view the existing data.
+DELETE the record from album where album_id is 1 and then hit 'Submit Answer'.
+*/
+
+DELETE the record from album where album_id is 1 and then hit 'Submit Answer'.
