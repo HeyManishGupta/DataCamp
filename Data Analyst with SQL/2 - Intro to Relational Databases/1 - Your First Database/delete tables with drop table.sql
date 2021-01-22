e0368e5f84cea4bfdeeeae8ff2f6467ac8be8f13
Manish Kumar Gupta <44649787+YourManishGupta@users.noreply.github.com>
2021-01-22 13:04:27 +0530

    Create delete tables with drop table.sql

---
+++ Data Analyst with SQL/2 - Intro to Relational Databases/1 - Your First Database/delete tables with drop table.sql
@@ -0,0 +1,14 @@
+/*
+Obviously, the university_professors table is now no longer needed and can safely be deleted.
+
+For table deletion, you can use the simple command:
+
+DROP TABLE table_name;
+
+Instructions
+------------
+Delete the university_professors table.
+*/
+
+-- Delete the university_professors table
+drop table university_professors;
