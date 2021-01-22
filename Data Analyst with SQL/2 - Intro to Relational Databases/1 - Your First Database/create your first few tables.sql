7e0f972e65ba8ac8d5f2d4cac0c0f74d7d6a6a2e
Manish Kumar Gupta <44649787+YourManishGupta@users.noreply.github.com>
2021-01-22 12:47:05 +0530

    Create create your first few tables.sql

---
+++ Data Analyst with SQL/2 - Intro to Relational Databases/1 - Your First Database/create your first few tables.sql
@@ -0,0 +1,38 @@
+/*
+You'll now start implementing a better database model. For this, you'll create tables for the professors and universities entity types. The other tables will be created for you.
+
+The syntax for creating simple tables is as follows:
+
+CREATE TABLE table_name (
+ column_a data_type,
+ column_b data_type,
+ column_c data_type
+);
+Attention: Table and columns names, as well as data types, don't need to be surrounded by quotation marks.
+
+Instructions
+------------
+Create a table professors with two text columns: firstname and lastname.
+Create a table universities with three text columns: university_shortname, university, and university_city
+*/
+
+-- Create a table for the professors entity type
+CREATE TABLE professors (
+ firstname text,
+ lastname text  
+);
+
+-- Print the contents of this table
+SELECT * 
+FROM professors;
+----------------------
+-- Create a table for the universities entity type
+create table universities (
+    university_shortname text,
+    university text,
+    university_city text
+);
+
+-- Print the contents of this table
+SELECT * 
+FROM universities;
